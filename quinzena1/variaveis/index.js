1635f4113f74f737c576c0aceca913d3e48d09e0
ninandre <andre-cecapi@hotmail.com>
2021-09-08 22:31:17 -0300

    Trabalha Aula 03 - Variáveis

---
+++ quinzena1/variaveis/index.js
@@ -0,0 +1,59 @@
+// let a = 10
+// let b = 10
+
+// console.log(b) // Vai aparecer o valor de B (10)
+
+// b = 5
+// console.log(a , b) //meu B passou de 10 pra 5
+
+// let a = 10
+// let b = 20
+// let c 
+// c = a //  c: 10
+// b = c // b: 10
+// a = b // a: 10 
+
+// console.log (a,b,c) // Alterei os valores do meu let, a, b e c 
+
+// let horasTrabalhadas = prompt("Quantas horas você trabalha por dia ?")
+// let dinheiroPorDia = prompt("Quanto você recebe por dia?")
+
+
+// alert(`Você recebe ${dinheiroPorDia/horasTrabalhadas} por hora`)
+
+// let nome;
+// let idade;
+
+// console.log(typeof(nome))
+// console.log(typeof(idade)) // por que minha variavel não tinha valor definido
+
+// nome = prompt("Qual é o seu nome")
+// idade = prompt("Qual é a sua idade")
+
+// console.log(typeof(nome))
+// console.log(typeof(idade)) // Colocamos um valor na minha variavel 
+
+// alert(`Olá ${nome}, você tem ${idade} anos`);
+
+// let primeiraPergunta = prompt("Vai pro shopping ? ")
+// let segundaPergunta = prompt ("Gosta de sorvete?")
+// let terceiraPergunta = prompt("Você joga lol")
+
+// alert(`Vai pro shopping ? ${primeiraPergunta}`)
+// alert(`Gosta de sorvete ? ${segundaPergunta}`)
+// alert(`Você joga lol ? ${terceiraPergunta}`)
+
+let a = 10
+let b = 25
+
+let c;
+c = b 
+b = a 
+a = c 
+
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
+
+
+
+
